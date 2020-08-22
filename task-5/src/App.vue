05ebe1a2de591ec40338ca606911ba0b178941fc
Sergey Koltsov <inser7@gmail.com>
2020-08-23 01:19:07 +0300

    init vue

---
+++ task-5/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="jumbotron text-center">
+      <h1>Олимпиада</h1>
+    </div>
+  
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
